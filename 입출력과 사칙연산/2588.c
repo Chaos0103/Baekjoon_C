6195d44ad77e60f47328464415125aa67ff1cf03
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-06-16 21:59:32 +0900

    Create 2588.c

---
+++ 입출력과 사칙연산/2588.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+	int num_1, num_2;
+	scanf("%d %d", &num_1, &num_2);
+	
+	printf("%d\n", num_1 * (num_2 % 10));
+	printf("%d\n", num_1 * (num_2 / 10 % 10));
+	printf("%d\n", num_1 * (num_2 / 100));
+	printf("%d\n", num_1 * num_2);
+}
