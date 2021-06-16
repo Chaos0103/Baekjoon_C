6dc86e96b284a137bfeb8bad2a9e95b903fc7a2f
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-06-16 22:36:52 +0900

    Create 11022.c

---
+++ for문/11022.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	int cnt;
+	int num_1, num_2;
+
+	scanf("%d", &cnt);
+
+	for (int i = 0; i < cnt; i++) {
+		scanf("%d %d", &num_1, &num_2);
+
+		printf("Case #%d: %d + %d = %d\n", i + 1, num_1, num_2, num_1 + num_2);
+	}
+}
