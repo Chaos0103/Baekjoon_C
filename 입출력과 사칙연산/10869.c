973eb1f89814c269bf9b19c5132754e5b5985a8c
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-06-16 21:53:29 +0900

    Create 10869.c

---
+++ 입출력과 사칙연산/10869.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+	int num_1, num_2;
+	scanf("%d %d", &num_1, &num_2);
+  
+	printf("%d\n", num_1 + num_2);
+	printf("%d\n", num_1 - num_2);
+	printf("%d\n", num_1 * num_2);
+	printf("%d\n", num_1 / num_2);
+	printf("%d\n", num_1 % num_2);
+}
